a1ed7a63047f1905fe6dca3a8711e56888e0caa1
cglotr <amir.ariffin.1992@gmail.com>
2022-09-06 06:02:59 +0800

    index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,23 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+import Container from '@mui/material/Container'
+import Link from 'next/link'
+import MuiLink from '@mui/material/Link'
+import Divider from '@mui/material/Divider'
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Unstable_Grid2'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <GoogleOAuthProvider
       clientId='454337127208-8cfsr6ebdake7qjp93n98rlrjjm9qgo6.apps.googleusercontent.com'
     >
-      <Component {...pageProps} />
+      <Container>
+        <Link href='/'><a>index</a></Link>
+        <Divider></Divider>
+        <Component {...pageProps} />
+      </Container>
     </GoogleOAuthProvider>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,41 @@
 import Link from 'next/link'
 import type { NextPage } from 'next'
+import MuiLink from '@mui/material/Link'
+import Typography from '@mui/material/Typography'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props: any) => {
   return (
     <>
-      <div>home page</div>
-      <Link href='/login'><button>login</button></Link>
+      <List>
+        {props.surahs.map((surah: any) => {
+          return (
+            <ListItem><Link href={`/surahs/${surah.id}`}><MuiLink>{`${surah.id} ${surah.title}`}</MuiLink></Link></ListItem>
+          )
+        })}
+      </List>
     </>
   )
 }
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const resp = await fetch('https://koran-backend-production.up.railway.app/')
+  const json = await resp.json()
+  const surahInfos = json.data.surah_infos
+  const surahs = surahInfos.map((surahInfo: any) => {
+    return {
+      id: surahInfo.surah_id,
+      title: surahInfo.title
+    }
+  })
+  return {
+    props: {
+      surahs
+    },
+  }
+}
+
 
 export default HomePage
