ce507bbb6441548bac4c583d0eb2ea1db04c55e2
cglotr <amir.ariffin.1992@gmail.com>
2022-09-06 04:55:38 +0800

    mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
