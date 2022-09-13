2b2626fcc26f275a767ccc7fb8b75efb4a0c9ff7
cglotr <amir.ariffin.1992@gmail.com>
2022-09-13 10:29:38 +0800

    gtm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -47,18 +47,15 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Script
-        src='https://www.googletagmanager.com/gtag/js?id=G-H3YY66J738'
-      />
-      <Script
-        id='google-analytics'
+        id='gtm'
         dangerouslySetInnerHTML={{
           __html:
             `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-H3YY66J738');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MCCNJVK');
             `
         }}
       />
