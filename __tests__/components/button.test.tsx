9a3d711a0d156c89ac4eef4da2b1c17ee36d2c25
cglotr <amir.ariffin.1992@gmail.com>
2022-09-13 12:11:50 +0800

    test button

---
+++ __tests__/components/button.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import { Button } from '../../src/components/button'
+
+describe('Button', () => {
+  test('clicking on it', async () => {
+    const user = userEvent.setup()
+    const mockFn = jest.fn()
+
+    render(
+      <Button
+        onClick={mockFn}
+      >
+        Google please hire me!
+      </Button>
+    )
+
+    const button = await screen.findByText('Google please hire me!')
+    expect(button).toBeInTheDocument()
+
+    await user.click(button)
+    expect(mockFn).toBeCalledTimes(1)
+  })
+
+  test('clicking on disabled button', async () => {
+    const user = userEvent.setup()
+    const mockFn = jest.fn()
+
+    render(
+      <Button
+        onClick={mockFn}
+        disabled={true}
+      >
+        Google please hire me!
+      </Button>
+    )
+
+    const button = await screen.findByText('Google please hire me!')
+    expect(button).toBeInTheDocument()
+
+    await user.click(button)
+    expect(mockFn).toBeCalledTimes(0)
+  })
+})
