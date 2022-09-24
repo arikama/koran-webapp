8dcdf79185fa302ead92b7987c41f621c7433f41
cglotr <amir.ariffin.1992@gmail.com>
2022-09-25 00:12:30 +0800

    add shortcut test

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -5,13 +5,13 @@ import userEvent from '@testing-library/user-event'
 
 import IndexPage, { getStaticProps } from './../../src/pages/index'
 
-import type { Props } from './../../src/pages/index'
+import type { SurahInfo } from '../../src/types/surah_info'
 
 const useRouter = jest.spyOn(require('next/router'), 'useRouter')
 
 describe('IndexPage', () => {
   test('render', async () => {
-    const result = await getStaticProps({}) as unknown as { props: Props }
+    const result = await getStaticProps({}) as unknown as { props: { surahInfos: SurahInfo[] } }
     render(<IndexPage surahInfos={result.props.surahInfos} />)
 
     expect(((await screen.findAllByText('1')).length)).not.toEqual(0)
@@ -31,10 +31,26 @@ describe('IndexPage', () => {
       }
     })
 
-    const result = await getStaticProps({}) as unknown as { props: Props }
+    const result = await getStaticProps({}) as unknown as { props: { surahInfos: SurahInfo[] } }
     render(<IndexPage surahInfos={result.props.surahInfos} />)
 
     await userEvent.click(await screen.findByText('The Opening'))
     expect(pushMock).toBeCalledWith('/surahs/1')
+  })
+
+  test('clicking shortcut', async () => {
+    const pushMock = jest.fn()
+    useRouter.mockImplementation(() => {
+      return {
+        push: pushMock
+      }
+    })
+
+    const result = await getStaticProps({}) as unknown as { props: { surahInfos: SurahInfo[] } }
+    render(<IndexPage surahInfos={result.props.surahInfos} />)
+
+    const found = await screen.findAllByText('114')
+    await userEvent.click(found[0])
+    expect(pushMock).toBeCalledWith('/#114')
   })
 })
