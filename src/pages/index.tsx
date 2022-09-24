8a6a78b8ecf628e17f04b1c5b0dad0c4c6effc2b
cglotr <amir.ariffin.1992@gmail.com>
2022-09-25 00:01:32 +0800

    refactor index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,69 +1,88 @@
-import { GetStaticProps, InferGetStaticPropsType } from 'next'
-import type { NextPage } from 'next'
-import { useRouter } from 'next/router'
+import { GetStaticProps, InferGetStaticPropsType } from "next"
+import type { NextPage } from "next"
+import { useRouter } from "next/router"
 
-import { Button } from './../components/button'
-import { KoranApiImpl } from './../apis/koran_api_impl'
-import { QuranText } from '../components/quran_text'
-import { getSurahSettings } from '../utils/get_surah_settings'
+import { Button } from "./../components/button"
+import { FONT } from "../constants/font"
+import { KoranApiImpl } from "./../apis/koran_api_impl"
+import { QuranText } from "../components/quran_text"
+import { getSurahSettings } from "../utils/get_surah_settings"
 
-import type { KoranApi } from './../apis/koran_api'
-import type { SurahInfo } from './../types/surah_info'
+import type { KoranApi } from "./../apis/koran_api"
+import type { SurahInfo } from "./../types/surah_info"
 
-export type Props = {
-  surahInfos: SurahInfo[]
-}
+const SURAH_TITLE_HEIGHT = 70
+const SURAH_SHORTCUTS = [25, 50, 75, 100, 114]
 
 const IndexPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (props) => {
   const router = useRouter()
   const surahSettings = getSurahSettings()
 
-  const SurahInfos = props.surahInfos.map(surahInfo => {
-    const href = `/surahs/${surahInfo.surahId}`
-    const onClick = () => {
-      router.push(`/#${surahInfo.surahId.toString()}`)
-      router.push(href)
-    }
+  const renderSurahIndex = () => {
+    return props.surahInfos.map(surahInfo => {
+      const href = `/surahs/${surahInfo.surahId}`
 
-    return (
-      <div
-        id={surahInfo.surahId.toString()}
-        key={surahInfo.surahId.toString()}
-        style={{
-          display: "flex",
-          alignItems: "center",
-          height: "70px"
-        }}
-      >
-        {surahInfo.surahId}
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <Button
-          onClick={onClick}
+      const onClick = () => {
+        router.push(`/#${surahInfo.surahId.toString()}`)
+        router.push(href)
+      }
+
+      const renderSurahId = () => {
+        return (
+          <div>{surahInfo.surahId}&nbsp;&nbsp;&nbsp;&nbsp;</div>
+        )
+      }
+
+      const renderSurahEnglishTitleButton = () => {
+        return (
+          <Button
+            onClick={onClick}
+            style={{
+              flex: 1,
+              textAlign: "left",
+              height: "100%",
+              cursor: "pointer"
+            }}
+          >
+            {surahSettings?.hideTranslation ? null : surahInfo.titleEnglish}
+          </Button>
+        )
+      }
+
+      const renderSurahArabicTitleButton = () => {
+        return (
+          <Button
+            onClick={onClick}
+            style={{
+              textDecoration: "none",
+              cursor: "pointer"
+            }}
+          >
+            {surahSettings?.hideVerse ? null : <QuranText text={surahInfo.titleArabic} />}
+          </Button>
+        )
+      }
+
+      return (
+        <div
+          id={surahInfo.surahId.toString()}
+          key={surahInfo.surahId.toString()}
           style={{
-            flex: 1,
-            textAlign: "left",
-            height: "100%",
-            cursor: "pointer"
+            display: "flex",
+            alignItems: "center",
+            height: `${SURAH_TITLE_HEIGHT}px`
           }}
         >
-          {surahSettings?.hideTranslation ? null : surahInfo.titleEnglish}
-        </Button>
-        <Button
-          onClick={onClick}
-          style={{
-            textDecoration: "none",
-            cursor: "pointer"
-          }}
-        >
-          {surahSettings?.hideVerse ? null : <QuranText text={surahInfo.titleArabic} />}
-        </Button>
-      </div>
-    )
-  })
+          {renderSurahId()}
+          {renderSurahEnglishTitleButton()}
+          {renderSurahArabicTitleButton()}
+        </div>
+      )
+    })
+  }
 
   const renderShortcuts = () => {
-    const shortcuts = [25, 50, 75, 100, 114]
-    const buttons = shortcuts.map((surahId => {
+    const buttons = SURAH_SHORTCUTS.map((surahId => {
       const href = `/#${surahId}`
       return (
         <Button
@@ -81,7 +100,7 @@ const IndexPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (pro
         style={{
           display: "flex",
           justifyContent: "space-between",
-          fontSize: "0.9em"
+          fontSize: FONT.FONT_SIZE_S
         }}
       >
         {buttons}
@@ -92,12 +111,14 @@ const IndexPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (pro
   return (
     <>
       {renderShortcuts()}
-      {SurahInfos}
+      {renderSurahIndex()}
     </>
   )
 }
 
-export const getStaticProps: GetStaticProps<Props> = async () => {
+export const getStaticProps: GetStaticProps<{
+  surahInfos: SurahInfo[]
+}> = async () => {
   const koranApi: KoranApi = new KoranApiImpl()
 
   const surahInfos = await koranApi.getSurahInfos()
